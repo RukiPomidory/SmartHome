b7d957911a93e207b2f2574b9bfa03d841c00745
RukiPomidory <RukiPomidory@yandex.ru>
2018-12-28 02:57:58 +0300

    trying connect to ble module

--- Mobile/Android/app/src/main/java/com/freshwind/smarthome/MainActivity.java
+++ Mobile/Android/app/src/main/java/com/freshwind/smarthome/MainActivity.java
@@ -1,6 +1,16 @@
 package com.freshwind.smarthome;
 
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.BluetoothSocket;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
@@ -8,15 +18,95 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button connectBtn;
+    private static final String TAG = "bluetooth1";
 
-    BluetoothAdapter btAdapter;
-    BluetoothSocket btSocket;
+    Button connectBtn;
+    Switch onOffSwitch;
+
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+
+    public final static String ACTION_GATT_CONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+    private BluetoothGatt GATT;
+
+    final BluetoothGattCallback callback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState)
+        {
+            super.onConnectionStateChange(gatt, status, newState);
+
+            String intentAction;
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                intentAction = ACTION_GATT_CONNECTED;
+                //mConnectionState = STATE_CONNECTED;
+                broadcastUpdate(intentAction);
+                Log.i(TAG, "Connected to GATT server.");
+                Log.i(TAG, "Attempting to start service discovery:" +
+                        GATT.discoverServices());
+
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                intentAction = ACTION_GATT_DISCONNECTED;
+                //mConnectionState = STATE_DISCONNECTED;
+                Log.i(TAG, "Disconnected from GATT server.");
+                broadcastUpdate(intentAction);
+
+            }
+
+        }
+
+        @Override
+        // При обнаружении нового сервиса
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
+            } else {
+                Log.w(TAG, "onServicesDiscovered received: " + status);
+            }
+        }
+
+        @Override
+        // Результат чтения характеристики
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic characteristic,
+                                         int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS)
+            {
+                broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+            }
+            else
+            {
+                Log.e(TAG, "failed character read");
+            }
+        }
+    };
+
+
+    // default UUID
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     // MAC-адрес Bluetooth модуля
     private static String address = "A8:1B:6A:75:9E:17";
@@ -27,24 +117,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        connectBtn = findViewById(R.id.connectBtn);
+        onOffSwitch = findViewById(R.id.onOff);
+
         btAdapter = BluetoothAdapter.getDefaultAdapter();
-        //checkBTState();
+        checkBTState();
 
         connectBtn.setOnClickListener(new OnClickListener() {
             public void onClick(View view)
             {
-
+                checkBTState();
             }
         });
 
-        sendData("");
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(onOffSwitch.isChecked())
+                {
+                    sendData("O");
+                }
+                else
+                {
+                    sendData("B");
+                }
+            }
+        });
+
     }
 
     private void sendData(String message)
     {
         byte[] msgBuffer = message.getBytes();
 
-        Log.d("TAG?", "...Посылаем данные: " + message + "...");
+        Log.d(TAG, "...Посылаем данные: " + message + "...");
 
         try
         {
@@ -56,5 +162,143 @@ public class MainActivity extends AppCompatActivity {
 
             errorExit("Fatal Error", msg);
         }
+    }
+
+    private void errorExit(String title, String message)
+    {
+        Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        Log.d(TAG, "onResume");
+
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try
+        {
+            GATT = device.connectGatt(this, false, callback);
+            GATT.connect();
+            Log.d(TAG, "GATT connected successfully");
+
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        }
+        catch (IOException e)
+        {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+
+        // Establish the connection.  This will block until it connects.
+        Log.d(TAG, "...Соединяемся...");
+        try
+        {
+            btSocket.connect();
+            Log.d(TAG, "Соединение успешно установлено");
+        }
+        catch (IOException e)
+        {
+            try
+            {
+                btSocket.close();
+                Log.d(TAG, "Ошибка соединения: " + e.getMessage());
+            }
+            catch (IOException e2)
+            {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+
+        // Create a data stream so we can talk to server.
+        Log.d(TAG, "...Создание Socket...");
+
+        try
+        {
+            outStream = btSocket.getOutputStream();
+            //sendData("O");
+            connectBtn.setBackgroundColor(Color.GREEN);
+            //sendData("F");
+        }
+        catch (IOException e)
+        {
+            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+        }
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        Log.d(TAG, "...In onPause()...");
+
+        if (outStream != null)
+        {
+            try
+            {
+                outStream.flush();
+            }
+            catch (IOException e)
+            {
+                errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+
+        try
+        {
+            btSocket.close();
+        }
+        catch (IOException e2)
+        {
+            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+
+    private void checkBTState()
+    {
+        // Check for Bluetooth support and then check to make sure it is turned on
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter == null)
+        {
+            errorExit("Fatal Error", "Bluetooth не поддерживается");
+        }
+        else
+        {
+            if (btAdapter.isEnabled())
+            {
+                Log.d(TAG, "...Bluetooth включен...");
+            }
+            else
+            {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    private void broadcastUpdate(final String action)
+    {
+        final Intent intent = new Intent(action);
+        sendBroadcast(intent);
+    }
+
+    private void broadcastUpdate(final String action,
+                                 final BluetoothGattCharacteristic character)
+    {
+        final Intent intent = new Intent(action);
+
+
+        Log.d(TAG, character.getUuid().toString());
+
+
+        sendBroadcast(intent);
     }
 }
