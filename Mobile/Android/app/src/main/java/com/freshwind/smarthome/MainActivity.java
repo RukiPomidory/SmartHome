1114007628ceae2e6fd87ec204268459474ebb06
RukiPomidory <RukiPomidory@yandex.ru>
2019-01-06 23:37:06 +0300

    newDevice button is working

--- Mobile/Android/app/src/main/java/com/freshwind/smarthome/MainActivity.java
+++ Mobile/Android/app/src/main/java/com/freshwind/smarthome/MainActivity.java
@@ -1,23 +1,48 @@
 package com.freshwind.smarthome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "CHOOSING";
     private final static String savedDevicesDir = "devices";
+
+    private TableLayout table;
+    private LayoutInflater inflater;
+    private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        inflater = getLayoutInflater();
+        table = findViewById(R.id.devices_table);
+        addButton = findViewById(R.id.add_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent scan = new Intent(MainActivity.this, ScanActivity.class);
+
+                startActivity(scan);
+            }
+        });
 
         try
         {
@@ -40,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 add(device);
             }
         }
-        catch (IOException exc)
+        catch (IOException | NullPointerException exc)
         {
             exc.printStackTrace();
         }
@@ -49,6 +74,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void add(Kettle device)
     {
+        TableRow row = (TableRow) inflater.inflate(R.layout.devices_table_row, null);
+        View deviceView = inflater.inflate(R.layout.devices_table_row, null);
 
+        row.addView(deviceView);
+
+        table.addView(row);
     }
 }
