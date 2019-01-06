57d001c0376629c299a58aee6f56c0e7fb308508
RukiPomidory <RukiPomidory@yandex.ru>
2019-01-06 18:31:27 +0300

    kettle icons and started creating launch activity

--- Mobile/Android/app/src/main/java/com/freshwind/smarthome/StartActivity.java
+++ Mobile/Android/app/src/main/java/com/freshwind/smarthome/StartActivity.java
@@ -1,8 +1,37 @@
 package com.freshwind.smarthome;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class StartActivity extends AppCompatActivity
 {
+    private final static String TAG = "LAUNCH";
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        try
+        {
+            getActionBar().hide();
+            Log.w(TAG, "getActionBar() OK");
+        }
+        catch (NullPointerException exc)
+        {
+            Log.w(TAG, "getActionBar() is null");
+        }
+
+        try
+        {
+            getSupportActionBar().hide();
+            Log.w(TAG, "getSupportActionBar() OK");
+        }
+        catch (NullPointerException exc)
+        {
+            Log.w(TAG, "getSupportActionBar() is null");
+        }
+    }
 }
