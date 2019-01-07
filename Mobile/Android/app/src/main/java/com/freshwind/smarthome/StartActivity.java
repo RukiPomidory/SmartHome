c2f6385b88a8b847c02d403b3f7e0db9a517dced
RukiPomidory <RukiPomidory@yandex.ru>
2019-01-07 07:05:08 +0300

    launch activity

--- Mobile/Android/app/src/main/java/com/freshwind/smarthome/StartActivity.java
+++ Mobile/Android/app/src/main/java/com/freshwind/smarthome/StartActivity.java
@@ -1,5 +1,6 @@
 package com.freshwind.smarthome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,25 +15,11 @@ public class StartActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_layout);
-
-        try
-        {
-            getActionBar().hide();
-            Log.w(TAG, "getActionBar() OK");
-        }
-        catch (NullPointerException exc)
-        {
-            Log.w(TAG, "getActionBar() is null");
-        }
-
-        try
-        {
             getSupportActionBar().hide();
             Log.w(TAG, "getSupportActionBar() OK");
-        }
-        catch (NullPointerException exc)
-        {
-            Log.w(TAG, "getSupportActionBar() is null");
-        }
+
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+        finish();
     }
 }
